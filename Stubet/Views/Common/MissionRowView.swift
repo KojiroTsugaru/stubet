d96e7767e3648a5b5e9709c9d23d78eba42a601b
Kojiro Tsugaru <kojirotsugaru@gmail.com>
2024-09-04 17:52:27 +0900

    added linear gradient to missionRowView

--- Stubet/Views/Common/MissionRowView.swift
+++ Stubet/Views/Common/MissionRowView.swift
@@ -13,31 +13,53 @@ struct MissionRowView: View {
     
     var body: some View {
         HStack {
-            VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 8) {
+                // Mission Title
                 Text(mission.title)
                     .font(.headline)
+                    .fontWeight(.bold)
                     .foregroundColor(isNew ? Color.white : Color.primary)
-                Text("16 時間")
-                    .font(.subheadline)
-                    .foregroundColor(isNew ? Color.white.opacity(0.7) : Color.secondary)
                 
-                HStack {
+                // Time Remaining
+                Text("16 時間後")
+                    .font(.subheadline)
+                    .foregroundColor(isNew ? Color.white : Color.secondary)
+                
+                // Location and Distance
+                HStack(spacing: 4) {
                     Image(systemName: "mappin.and.ellipse")
                     Text(mission.location.name)
-                    Text("1 km")
+                    Text("4.5 km")
                 }
                 .font(.caption)
-                .foregroundColor(isNew ? Color.white.opacity(0.7) : Color.secondary)
+                .foregroundColor(isNew ? Color.white : Color.secondary)
             }
             Spacer()
-            Image("profileImage")
+            
+            // Profile Image
+            Image("DummyProfileImage")
                 .resizable()
+                .aspectRatio(contentMode: .fill)
                 .frame(width: 40, height: 40)
                 .clipShape(Circle())
         }
         .padding()
-        .background(isNew ? Color.orange : Color(UIColor.systemBackground))
+        .background(isNew ?
+                    AnyView(LinearGradient(
+                        stops: [
+                            Gradient.Stop(color: Color(red: 1.00, green: 0.75, blue: 0.29), location: 0.00),
+                            Gradient.Stop(color: Color(red: 1.00, green: 0.62, blue: 0.29), location: 1.00),
+                        ],
+                        startPoint: UnitPoint(x: 1, y: 0),
+                        endPoint: UnitPoint(x: 0, y: 1)
+                    ))
+                    : AnyView(Color(UIColor.systemBackground)))
         .cornerRadius(10)
-        .shadow(radius: 1)
+        .shadow(radius: 2)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(isNew ? Color.orange.opacity(0) : Color.gray.opacity(0.2), lineWidth: 1)
+        )
+        
     }
 }
