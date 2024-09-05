7ae8ad26b6a3b1e4895351ec7c13eb25b3f336cc
Kojiro Tsugaru <kojirotsugaru@gmail.com>
2024-09-06 03:03:41 +0900

    UserLocationManager made

--- Stubet/StubetApp.swift
+++ Stubet/StubetApp.swift
@@ -9,12 +9,12 @@ import SwiftUI
 import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
 }
 
 
@@ -22,13 +22,15 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 struct StubetApp: App {
     let persistenceController = PersistenceController.shared
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
+    
     var body: some Scene {
         WindowGroup {
-           ContentView()
-               .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            //           ContentView()
+            //               .environment(\.managedObjectContext, persistenceController.container.viewContext)
             // SignupView()
             // LoginView()
+            LocationTestView()
+                .environmentObject(UserLocationManager()) // Inject real location manager in the live app
         }
     }
 }
