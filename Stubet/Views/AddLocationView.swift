b9ee6d4b034c5034d35a0a6e623f570321ab890d
Kojiro Tsugaru <kojirotsugaru@gmail.com>
2024-09-05 20:40:13 +0900

    adjusted UI

--- Stubet/Views/AddLocationView.swift
+++ Stubet/Views/AddLocationView.swift
@@ -4,42 +4,55 @@
 //
 //  Created by KJ on 9/5/24.
 //
-
 import SwiftUI
 import MapKit
 
 struct AddLocationView: View {
     @StateObject private var viewModel = AddLocationViewModel()
     
+    @State var locationName = ""
+    
     var body: some View {
         VStack {
+            MapSearchBar(text: $viewModel.searchText, onSearchButtonClicked: {
+                viewModel.searchForLocation()
+            })
+            
+            // Map View
             Map(coordinateRegion: $viewModel.region, interactionModes: .all, annotationItems: viewModel.selectedCoordinates) { coord in
-                MapPin(coordinate: coord.coordinate)
+                MapMarker(coordinate: coord.coordinate)
             }
-            .frame(height: 300)
+            .cornerRadius(16)
             .onTapGesture {
                 let coordinate = viewModel.region.center
                 viewModel.selectLocation(coordinate: coordinate)
             }
             
-            MapSearchBar(text: $viewModel.searchText, onSearchButtonClicked: {
-                viewModel.searchForLocation()
-            })
-            
-            if let coord = viewModel.selectedCoordinates.first?.coordinate {
-                VStack(alignment: .leading) {
-                    Text("Selected Location:")
-                    Text("Latitude: \(coord.latitude)")
-                    Text("Longitude: \(coord.longitude)")
-                }
-                .padding()
+            // Location Name Field
+            VStack(alignment: .leading, spacing: 8) {
+                Text("場所の名前")
+                    .font(.system(size: 16, weight: .bold))
+                    .padding(.leading, 16)
+                
+                TextField("例: 千葉大学１号館", text: $locationName)
+                    .padding()
+                    .background(Color(UIColor.systemGray6))
+                    .cornerRadius(10)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 1))
+                    .padding(.horizontal, 16)
             }
-            
-            Spacer()
+            .padding(.vertical, 16)
         }
         .navigationTitle("場所を設定")
     }
 }
+
+struct AddLocationView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddLocationView()
+    }
+}
+
 
 
 
