667ca0f6529cb7b16726dc28b968ed2fdfa123a2
Kojiro Tsugaru <kojirotsugaru@gmail.com>
2024-09-05 23:16:03 +0900

    added UI

--- Stubet/ContentView.swift
+++ Stubet/ContentView.swift
@@ -58,7 +58,6 @@ struct ContentView: View {
 //                        Text("プロフィール")
 //                    }
             }
-            .accentColor(Color.orange)
-        }
+        }.accentColor(Color.orange)
     }
 }
