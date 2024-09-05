3afeada38904f9a75c475b6f636b99615482348d
Kojiro Tsugaru <kojirotsugaru@gmail.com>
2024-09-05 13:41:01 +0900

    added kijima's change

--- Stubet/Models/FriendModel.swift
+++ Stubet/Models/FriendModel.swift
@@ -7,17 +7,27 @@
 
 import Foundation
 
-struct Friend: Identifiable {
+struct Friend: Identifiable, Hashable {
     let id: String
     let userName: String
     let displayName: String
     let iconUrl: String
-
+    
     // Initialize from Firebase document data
     init(id: String, data: [String: Any]) {
         self.id = id
         self.userName = data["userName"] as? String ?? ""
         self.displayName = data["displayName"] as? String ?? ""
         self.iconUrl = data["icon_url"] as? String ?? ""
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    // MARK: - Equatable
+    
+    static func == (lhs: Friend, rhs: Friend) -> Bool {
+        return lhs.id == rhs.id
     }
 }
