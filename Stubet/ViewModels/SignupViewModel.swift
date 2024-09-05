4ebff82a19a1fda644d32c501b301ab92bef48c5
Kadoshima <galaxykadosima@gmail.com>
2024-09-05 13:18:54 +0900

    [add]signupロジックの追加

--- Stubet/ViewModels/SignupViewModel.swift
+++ Stubet/ViewModels/SignupViewModel.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 import Combine
+import FirebaseAuth
+import Foundation
 
 class SignupViewModel: ObservableObject {
     @Published var username = ""
@@ -30,21 +32,58 @@ class SignupViewModel: ObservableObject {
     
     // バリデーションロジックを含むメソッド
     func validateFields() {
-        // 例として、各フィールドが空かどうかの簡単なバリデーション
+        // テキストボックスが空かどうか
         usernameError = username.isEmpty ? "Username is required." : ""
         emailError = email.isEmpty ? "Email is required." : ""
         passwordError = password.isEmpty ? "Password is required." : ""
         confirmPasswordError = confirmPassword.isEmpty ? "Confirmation is required." : ""
 
-        // その他のバリデーションもここに追加
+        // username条件
+        
+        // email条件
+        
+        // password条件
+        passwordError = (password.count < 8) ? "Password length error" : ""
+        
+        // confirmPassword条件
+        if password != confirmPassword{
+            confirmPasswordError = "Password not match"
+        }
+        
+        
     }
 
     func signup() {
         validateFields()
 
-        // エラーがなければ続行、エラーがあればshowErrorをtrueに設定
+        // エラーがなければ続行
         if usernameError.isEmpty && emailError.isEmpty && passwordError.isEmpty && confirmPasswordError.isEmpty {
-            // サインアップ処理
+            // Firebaseにユーザー登録
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let error = error {
+                    self.showError = true
+                    // エラーの詳細を表示するように変更
+                    self.errorMessage = "Failed to sign up: \(error.localizedDescription)"
+                    // エラーの詳細をコンソールに出力
+                    print("Error during signup: \(error)")
+                } else {
+                    // ユーザー登録成功時の処理
+                    self.showError = false
+                    self.errorMessage = ""
+                    
+                    // リフレッシュトークンの保存
+                    if let refreshToken = authResult?.user.refreshToken {
+                        UserDefaults.standard.set(refreshToken, forKey: "userRefreshToken")
+                    }
+                    
+                    print("User signed up successfully: \(authResult?.user.email ?? "")")
+
+                    // ログイン処理を完了させる（必要に応じて実装を調整）
+                    // 例: ログイン状態を管理するフラグを更新する、ユーザー情報を保存するなど
+                    
+                    // ホーム画面に遷移
+                }
+            }
         } else {
             showError = true
             errorMessage = "Please fix the errors above."
